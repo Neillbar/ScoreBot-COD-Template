c19eaa984c1fad734ada56ef17fe88e3184f35b7
swssr <simo@swssr.co>
2021-08-15 12:30:59 +0200

    Add scoreList and overallScore props

--- score-bot-ark-template/src/app/Core/Models/score-object.ts
+++ score-bot-ark-template/src/app/Core/Models/score-object.ts
@@ -1,56 +1,55 @@
-
 export interface Info {
-    openRound: string;
-    submissionChannel?: any;
-    members: number;
-    _id: string;
-    customPlacements: any[];
-    customPoints: any[];
-    serverID: string;
-    tournamentID: string;
-    open: boolean;
-    rounds: string;
-    type: string;
-    roleID: string;
-    pictureVerification: boolean;
-    registrationChannel: string;
-    customPointSystem: boolean;
-    leaderboardView: string;
-    __v: number;
+  openRound: string;
+  submissionChannel?: any;
+  members: number;
+  _id: string;
+  customPlacements: any[];
+  customPoints: any[];
+  serverID: string;
+  tournamentID: string;
+  open: boolean;
+  rounds: string;
+  type: string;
+  roleID: string;
+  pictureVerification: boolean;
+  registrationChannel: string;
+  customPointSystem: boolean;
+  leaderboardView: string;
+  __v: number;
 }
 
 export interface Player {
-    _id: string;
-    playerID: string;
-    Username: string;
-    score1?: number;
-    scorePlacement1?: string;
-    score2: number;
-    scorePlacement2?: string;
-    score3: number;
-    scorePlacement3?: string;
-    score4: number;
-    scorePlacement4?: string;
-    score5?: number;
-    scorePlacement5?: string;
-    score6?: number;
-    scorePlacement6?: string;
-    score7?: number;
-    scorePlacement7?: string;
-    score8?: number;
-    scorePlacement8?: string;
-    score9?: number;
-    scorePlacement9?: string;
-    score10?: number;
-    scorePlacement10?: string;
+  _id: string;
+  playerID: string;
+  Username: string;
+  score1?: number;
+  scorePlacement1?: string;
+  score2: number;
+  scorePlacement2?: string;
+  score3: number;
+  scorePlacement3?: string;
+  score4: number;
+  scorePlacement4?: string;
+  score5?: number;
+  scorePlacement5?: string;
+  score6?: number;
+  scorePlacement6?: string;
+  score7?: number;
+  scorePlacement7?: string;
+  score8?: number;
+  scorePlacement8?: string;
+  score9?: number;
+  scorePlacement9?: string;
+  score10?: number;
+  scorePlacement10?: string;
+
+  scoreList?: number[];
+  overallScore?: number;
 }
 
 export interface ScoreObject {
-    success: boolean;
-    type: string;
-    info: Info;
-    players: Player[];
+  success: boolean;
+  type: string;
+  info: Info;
+  players: Player[];
 }
-
-
-
