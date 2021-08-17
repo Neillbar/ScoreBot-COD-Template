d6749a953f0a14635d0b213aa68f3c015b3ef7fc
Simo Ngquseka <simo@swssr.co>
2021-08-17 18:19:59 +0200

    Add has key helper

--- score-bot-ark-template/src/app/Lib/helpers.ts
+++ score-bot-ark-template/src/app/Lib/helpers.ts
@@ -1,3 +1,6 @@
 export const isScoreValue = (value: string): boolean => {
   return value && /score\d/.test(value);
 };
+
+export const hasKey = (key: string, object: any) =>
+  key && Object.keys(object).some((k) => k === key);
